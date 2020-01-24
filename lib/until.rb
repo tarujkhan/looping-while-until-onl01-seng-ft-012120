a7316640ac2424b837bb07e791b468746b67643c
taroj khan <tarujkhan@yahoo.com>
2020-01-24 23:44:28 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,24 @@
+#def using_until
+  #levitation_force = 6
+  #your code here
+   #def using_until
+  #levitation_force = 6
+	
+	#until levitation_force < 10
+	 # 	puts "Wingardium Leviosa"
+	  	#your code helper_method
+	  #	levitation_force += 1
+	  	
+#end
+#end
+
 def using_until
   levitation_force = 6
-  #your code here
-    
+	
+	until levitation_force == 10
+	  	puts "Wingardium Leviosa"
+	  	#your code helper_method
+	  	levitation_force += 1
+	  	
 end
-
+end
